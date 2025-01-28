71f15b549f5caba9da8c35c0bbb442830e19745a
Lino-29 <lino_voxter@hotmail.com>
2025-01-28 14:27:27 -0600

    Primer commit respaldo

---
+++ FP_add-sub_class24-25/FP_add-sub_class24-25.srcs/sources_1/new/add_sub.sv
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: Cinvestav
+// Engineer: Daniel, Lino, Kevin, Emmanuel
+// 
+// Create Date: 22.01.2025 12:31:55
+// Design Name: Floating Point Adder and Subctractor
+// Module Name: add_sub
+// Project Name: Floating Point Adder and Subctractor
+// Target Devices: 
+// Tool Versions: 
+// Description: Adds and subtracts two inputs and delivers one outup following IEEE 754 format
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module add_sub #(parameter WIDTH = 32)(
+    input  [WIDTH-1:0] a,b,
+    input bit operation_select,
+    output [WIDTH-1:0] result,
+    output sign_a, sign_b, sign_result,
+    output [WIDTH-10:0] mantissa_a, mantissa_b, mantissa_result,
+    output [WIDTH-25:0] exp_a, exp_b, exp_result
+    );
+    
+    //Separate sign, exponent and mantissa values for both inputs
+    assign mantissa_a = a[22:0];
+    assign exp_a = a[30:23];
+    assign sign_a = a[31];
+        
+    assign mantissa_b = b[22:0];
+    assign exp_b = b[30:23];
+    assign sign_b = b[31];
+    
+endmodule
