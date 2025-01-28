31c84fbf01778491e6111f660cf428d6ddf3440e
KevAraujo <kevin.araujo@cinvestav.mx>
2025-01-28 16:50:18 -0600

    Update sign_logic.sv

--- FP_add-sub_class24-25/FP_add-sub_class24-25.srcs/sources_1/new/sign_logic.sv
+++ FP_add-sub_class24-25/FP_add-sub_class24-25.srcs/sources_1/new/sign_logic.sv
@@ -1,5 +1,5 @@
 module sign_logic #(parameter WIDTH = 32)(
-    input wire [WIDTH-1:0] X,Y,
+    input wire [WIDTH-1:0] x,y,
     input bit operation_select,
     //input bit clk,
     output reg [0:0] result,
@@ -11,18 +11,18 @@ module sign_logic #(parameter WIDTH = 32)(
     wire def1_1;
     wire sign_x, sign_y;
     
-    assign sign_x = X[31];
-    assign sign_y = Y[31];
+    assign sign_x = x[31];
+    assign sign_y = y[31];
     
-    assign mantisa_a = X[22:0];
-    assign exp_a = X[30:23];
+    assign mantisa_a = x[22:0];
+    assign exp_a = x[30:23];
         
     assign mantissa_b = b[22:0];
     assign exp_b = b[30:23];
     assign sign_b = b[31];
 
-    assign mantisa_b = Y[22:0];
-    assign exp_b = Y[30:23];
+    assign mantisa_b = y[22:0];
+    assign exp_b = y[30:23];
     assign sign_b = operation_select ? sign_y : ~sign_y;
     
     assign def1_1 = (mantisa_a > mantisa_b) ? sign_x : (mantisa_a < mantisa_b) ? sign_b : (sign_a ~^ sign_b);
